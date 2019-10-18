9a20478d1e4accc43244a64267d14b571775ffaf
shangyi lim <shangyilim@shangyis-MacBook-Air-2.local>
2019-10-18 09:33:09 +0800

    fix gallery not working on ios

--- src/components/gallery_block/index.js
+++ src/components/gallery_block/index.js
@@ -13,11 +13,11 @@ export default class GalleryBlock extends Component {
       this.io = new IntersectionObserver(
         entries => {
           const visibleEntries = entries.filter(e => e.isIntersecting);
-
+          
           visibleEntries
-            .filter(e => e.target instanceof HTMLImageElement)
+            .filter(e => e.target.dataset.src)
             .forEach(e => {
-              e.target.src = e.target.dataset.src;
+              e.target.style.backgroundImage = `url('${e.target.dataset.src}')`;
             });
         },
         {
@@ -44,46 +44,24 @@ export default class GalleryBlock extends Component {
   render() {
     return (
       <div class={style.photos_grid}>
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072861/devfestkl18_pwa/gallery/1.jpg"
-        />
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,h_372/v1564582278/IMG_2767.jpg"
-        />
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,w_393/v1564582115/DevFestKL18-6.jpg"
-        />
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,w_556/v1564582619/Group_Photo_-_1.jpg"
-        />
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,h_363/v1564582756/DSCF1242_1.jpg"
-        />
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072846/devfestkl18_pwa/gallery/6.jpg"
-        />
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072883/devfestkl18_pwa/gallery/7.jpg"
-        />
-        <img
-          crossorigin="anonymous"
-          class={style.grid_item}
-          data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072866/devfestkl18_pwa/gallery/8.jpg"
-        />
+     <div class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072861/devfestkl18_pwa/gallery/1.jpg">
+    </div>
+      <div class={style.grid_item} data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,h_372/v1564582278/IMG_2767.jpg">
+      </div>
+        <div class={style.grid_item} data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,w_393/v1564582115/DevFestKL18-6.jpg">
+        
+        </div>
+        <div class={style.grid_item} data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,w_556/v1564582619/Group_Photo_-_1.jpg">
+        </div>
+        <div class={style.grid_item} data-src="https://res.cloudinary.com/shangyilim/image/upload/c_scale,h_363/v1564582756/DSCF1242_1.jpg">
+        </div>
+        <div class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072846/devfestkl18_pwa/gallery/6.jpg">
+        </div>
+        <div class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072883/devfestkl18_pwa/gallery/7.jpg">
+        </div>
+        <div class={style.grid_item} data-src="https://res.cloudinary.com/limhenry/image/upload/v1537072866/devfestkl18_pwa/gallery/8.jpg">
+
+        </div>
         <div crossorigin="anonymous" class={style.gallery_info}>
           <div>
             <h2>DevFest KL 2018 highlights</h2>
